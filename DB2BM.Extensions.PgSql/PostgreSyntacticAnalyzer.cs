246015a1420032e47c59d7b82696c566fcd3b5f9
Annette Suarez Fernandez <annettesuarezfernandez@gmail.com>
2020-09-04 08:57:42 -0400

    Moved and updated diagrams Fixed and Renamed Parser Reordered command line options

--- DB2BM.Extensions.PgSql/PostgreSyntacticAnalyzer.cs
+++ DB2BM.Extensions.PgSql/PostgreSyntacticAnalyzer.cs
@@ -5,16 +5,16 @@ using System.Text;
 using DB2BM.Abstractions.Attrs;
 using DB2BM.Abstractions.Entities;
 using DB2BM.Abstractions.Interfaces;
+using DB2BM.Extensions.PgSql.Parser;
 
 namespace DB2BM.Extensions.PgSql
 {
     [Dbms("postgre")]
     public class PostgreSyntacticAnalyzer : ISyntacticAnalyzer
-    {
-
+    {        
         public void Parse(StoreProcedure sp)
         {
-            var parser = new Parser.FunctionDefinitionConstructor(sp.OriginalCode);
+            var parser = new SPParser(sp.OriginalCode);
             sp.AST = parser.GetAST();
         }
     }
